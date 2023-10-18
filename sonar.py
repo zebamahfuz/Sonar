7fc609f97f393ca48e5e07267f9d716b9dd8c676
zebamahfuz <zebamahfuz@gmail.com>
2023-10-18 11:56:51 +0100

    Update sonar.py

--- sonar.py
+++ sonar.py
@@ -7,15 +7,15 @@ year = 2000
 
 # divided by 100 means century year (ending with 00)
 # century year divided by 400 is leap year
-#if (year % 400 == 0) and (year % 100 == 0):
+if (year % 400 == 0) and (year % 100 == 0):
     print("{0} is a leap year".format(year))
 
 # not divided by 100 means not a century year
 # year divided by 4 is a leap year
-#elif (year % 4 ==0) and (year % 100 != 0):
+elif (year % 4 ==0) and (year % 100 != 0):
     print("{0} is a leap year".format(year))
 
 # if not divided by both 400 (century year) and 4 (not century year)
 # year is not leap year
-#else:
+else:
     print("{0} is not a leap year".format(year))
